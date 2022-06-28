2b582258ed3ec17aab39b00e965d160377113914
Jacob Yaw <yawjacob@yahoo.com>
2022-06-27 20:20:08 -0500

    finished all but last. Used slack for help. Study this syntax

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validate :clickbait?
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    BAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if BAIT_PATTERNS.none? { |pattern| pattern.match(title) }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
